c67b19969807735852046beb89c80e799a6b40fc
AmirAli <ososgol@gmail.com>
2026-06-24 18:58:47 +0330

    feat(auth): implement login, logout, and registration views

---
+++ user_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import LogoutView, LoginView, RegisterView
+
+app_name = 'user_app'
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+
 
 # Create your views here.
+
+
+class LoginView(View):
+    template_name = 'login.html' # not exist yet
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request=request,
+            username = username,
+            password = password
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        return render(request, self.template_name,{"error": "Invalid username or password."})
+
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect(reverse('user_app:login'))
+
+
+class RegisterView(View):
+    template_name = 'register.html' # not exist yet
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(username=username, password=password)
+        return redirect(reverse('user_app:login'))
+
+
