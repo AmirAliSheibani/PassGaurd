4cf39b0a3894adb98402611e2538301b7fdc2bb6
AmirAli <ososgol@gmail.com>
2026-07-27 21:02:37 +0330

    feat: add vault model with constraints and indexes

--- vault_app/models.py
+++ vault_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+from user_app.models import CustomUser
+
+
 # Create your models here.
+
+
+class Vault(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="vaults")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    is_default = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Vault"
+        verbose_name_plural = 'Vaults'
+        ordering = ['-created_at']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'name'], name='unique_user_vault_name'
+            ),
+        ]
+        indexes = [
+            models.Index(
+                fields=['user', '-created_at'],
+            )
+        ]
+
+
+    def __str__(self):
+        return f"{self.user.username}-{self.name}"
+
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.capitalize()
+        super(Vault, self).save(*args, **kwargs)
+
