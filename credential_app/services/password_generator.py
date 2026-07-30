f772709eaae3cd1f2aa15a998859031e9177e1ac
AmirAli <ososgol@gmail.com>
2026-07-30 21:41:15 +0330

    feat: implement password generator class in service layer

---
+++ credential_app/services/password_generator.py
@@ -0,0 +1,52 @@
+import secrets
+import string
+
+
+class PasswordGenerator:
+    """
+    Generates secure random passwords.
+    """
+
+    DEFAULT_LENGTH = 16
+
+    UPPERCASE = string.ascii_uppercase
+    LOWERCASE = string.ascii_lowercase
+    NUMBERS = string.digits
+    SYMBOLS = "!@#$%^&*_-+=."
+
+    @classmethod
+    def generate(cls, length: int = DEFAULT_LENGTH) -> str:
+        if length < cls.DEFAULT_LENGTH:
+            raise ValueError(
+                f"Password length must be at least {cls.DEFAULT_LENGTH} characters"
+            )
+
+        required = [
+            secrets.choice(cls.UPPERCASE),
+            secrets.choice(cls.LOWERCASE),
+            secrets.choice(cls.NUMBERS),
+            secrets.choice(cls.SYMBOLS),
+        ] # Example: ["K", "m", "7", "@"]
+
+        all_characters = (
+            cls.UPPERCASE
+            + cls.LOWERCASE
+            + cls.NUMBERS
+            + cls.SYMBOLS
+        ) # Result: ["ABCDE...abcde...01234...!@#$..."]
+
+        remaining = [
+            secrets.choice(all_characters)
+            for _ in range(length - len(required)) # 8 - 4
+        ]
+
+        password_chars = required + remaining # 4 + 4
+        shuffled_password = []
+
+        while password_chars:
+            random_index = secrets.randbelow(len(password_chars))
+            shuffled_password.append(
+                password_chars.pop(random_index)
+            )
+
+        return "".join(shuffled_password)
