45a7031f8dd1c786bea7de31dc9fbe5e6cdad571
AmirAli <ososgol@gmail.com>
2026-08-05 22:35:12 +0330

    feat(categories): introduce category selector layer

--- vault_app/selectors/vault_selector.py
+++ vault_app/selectors/vault_selector.py
@@ -31,3 +31,30 @@ class VaultSelector:
     def get_recent(cls, *, user_id: int) -> Vault:
         return Vault.objects.filter(user_id=user_id).order_by('-created_at').first()
 
+
+class CategorySelector:
+
+    @classmethod
+    def get_by_id(cls, *, category_id: int) -> Category:
+        return Category.objects.get(pk=category_id)
+
+    @classmethod
+    def get_by_id_for_user(cls, *, category_id: int, user_id: int) -> Category:
+        return Category.objects.get(pk=category_id, user_id=user_id)
+
+    @classmethod
+    def get_user_categories(cls, *, user_id: int) -> QuerySet[Category]:
+        return Category.objects.filter(user_id=user_id).order_by("name")
+
+    @classmethod
+    def exists_by_name(cls, *, category_name: int, user_id: int) -> bool:
+        return Category.objects.filter(name__iexact=category_name, user_id=user_id).exists()
+
+    @classmethod
+    def count(cls, *, user_id: int) -> int:
+        return Category.objects.filter(user_id=user_id).count()
+
+    @classmethod
+    def get_recent(cls, *, user_id: int) -> Category:
+        return Category.objects.filter(user_id=user_id).order_by('-created_at').first()
+
