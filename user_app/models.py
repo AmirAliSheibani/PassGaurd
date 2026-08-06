1e34482cf4439f16fbcd46698e3d31c17418c503
AmirAli <ososgol@gmail.com>
2026-08-06 18:52:40 +0330

    feat(users): implement custom user model

--- user_app/models.py
+++ user_app/models.py
@@ -4,6 +4,24 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    """
+    Custom user model for PassGuard.
 
+    The user model Only handles authentication
+    and account-level information.
+    """
+    recovery_setup_completed = models.BooleanField(
+        default=False,
+        help_text='Indicates whether the user recovery setup was completed.'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = [
+            "-created_at"
+        ]
+
+    def __str__(self):
+        return self.username
 
