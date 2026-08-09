c3037224a71100adee923faafa1fd25796c3a0c3
AmirAli <ososgol@gmail.com>
2026-08-09 20:54:32 +0330

    feat(users): introduce user selector layer

---
+++ user_app/selectors/user_selector.py
@@ -0,0 +1,23 @@
+from django.db.models import Q
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSelector:
+
+    @classmethod
+    def get_by_id(cls, *, user_id: int) -> User:
+        return User.objects.get(pk=user_id)
+
+    @classmethod
+    def get_by_username(cls, *, username: str) -> User:
+        return User.objects.get(username__iexact=username)
+
+    @classmethod
+    def get_by_id_if_active(cls, *, user_id: int) -> User:
+        return User.objects.get(pk=user_id, is_active=True)
+
+    @classmethod
+    def exist_by_username(cls, *, username: str) -> bool:
+        return User.objects.filter(username__iexact=username).exists()
