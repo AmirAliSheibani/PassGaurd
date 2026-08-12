fdfb1d9fcd4a91ac7fc438a0615a1ac4c3524267
AmirAli <ososgol@gmail.com>
2026-08-12 19:35:21 +0330

    feat(auth): add register view

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from core.views import home_view
 
 app_name = "core"
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name="home"),
 ]
--- vault_app/views.py
+++ vault_app/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from user_app.forms import LoginForm
+from user_app.forms import LoginForm, RegisterForm
 from user_app.models import BackupCode
+from user_app.services.user_services import UserService
+
+
 # Create your views here.
 
 
@@ -14,7 +17,7 @@ class LoginView(View):
 
     def get(self, request):
         if request.user.is_authenticated:
-            return redirect("/")
+            return redirect("core:home")
 
         form = LoginForm()
         return render(request, self.template_name, {'form': form})
@@ -41,10 +44,36 @@ class LoginView(View):
 class LogoutView(View):
     def get(self, request):
         logout(request)
-        return redirect("/")
+        return redirect("core:home")
 
     def post(self, request):
         logout(request)
 
-        return redirect("/")
+        return redirect("core:home")
+
+
+class RegisterView(View):
+    template_name = 'user_app/register.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect("core:home")
+
+        form = RegisterForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+
+        if not form.is_valid():
+            return render(request, self.template_name, {'form': form})
+
+        user = UserService.register(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password']
+        )
+        login(request, user)
+        return redirect("core:home")
+
+
 
