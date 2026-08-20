6a4158ac5984b2c995db85451173da8b15865486
AmirAli <ososgol@gmail.com>
2026-08-20 22:22:34 +0330

    feat(users): add reset master password form

--- user_app/views.py
+++ user_app/views.py
@@ -157,9 +157,6 @@ class BackupCodeRecoveryView(View):
         return redirect("user_app:reset-master-password")
 
 
-class ResetMasterPasswordView(View):
-    template_name = 'user_app/reset_master_password.html'
-
 
 
 
