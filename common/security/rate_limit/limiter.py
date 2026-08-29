41f9b10cf75df5884625331003b8df8d40f85c51
AmirAli <ososgol@gmail.com>
2026-08-29 22:48:20 +0330

    feat(rate limits): update policies.py

--- common/security/rate_limit/limiter.py
+++ common/security/rate_limit/limiter.py
@@ -111,3 +111,4 @@ class RateLimiter:
         )
 
         cls.cache.delete(key)
+
--- common/security/rate_limit/policies.py
+++ common/security/rate_limit/policies.py
@@ -2,18 +2,17 @@ class LoginRateLimitPolicy:
     IP_LIMIT = 20
     IP_WINDOW = 60
 
-    USERNAME_LIMIT = 5
-    USERNAME_WINDOW = 300
+    USERNAME_FAILURE_LIMIT = 5
+    USERNAME_FAILURE_WINDOW = 300
 
 
 class RecoveryRateLimitPolicy:
-    IP_LIMIT = 20
+    IP_LIMIT = 10
     IP_WINDOW = 300
 
-    USERNAME_LIMIT = 5
-    USERNAME_WINDOW = 300
+    USERNAME_FAILURE_LIMIT = 5
+    USERNAME_FAILURE_WINDOW = 300
 
 
 class BackupCodeRegenerationPolicy:
-    COOLDOWN = 300
-
+    COOLDOWN_SECONDS = 300
