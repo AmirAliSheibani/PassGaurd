608e37192a1c834effae8bc0c261085807e2c067
AmirAli <ososgol@gmail.com>
2026-09-06 21:09:08 +0330

    refactor(vault): update selectors method to use slug field

--- vault_app/selectors/vault_selector.py
+++ vault_app/selectors/vault_selector.py
@@ -1,5 +1,8 @@
 from vault_app.models import Vault, Category
 from django.db.models import QuerySet
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class VaultSelector:
 
@@ -10,6 +13,16 @@ class VaultSelector:
     @classmethod
     def get_by_id_for_user(cls, *, vault_id: int, user_id: int) -> Vault:
         return Vault.objects.get(pk=vault_id, user_id=user_id)
+
+    @classmethod
+    def get_by_username_and_slug_for_user(cls, *, username: str, vault_slug: str, user: User) -> Vault:
+        return (
+            Vault.objects.select_related("user").get(
+                user=user,
+                user__username=username,
+                slug=vault_slug,
+            )
+        )
 
     @classmethod
     def get_by_default(cls, *, user_id: int) :
