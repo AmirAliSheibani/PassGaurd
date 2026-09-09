8e5582b8efcef42bc9d16bd8d3a77704cbe5bd54
AmirAli <ososgol@gmail.com>
2026-09-09 21:18:57 +0330

    feat(vault): add classbase create and updates view

--- vault_app/views.py
+++ vault_app/views.py
@@ -40,4 +40,81 @@ class VaultDetailView(LoginRequiredMixin, VaultObjectMixin, TemplateView):
         return context
 
 
+class VaultCreateView(LoginRequiredMixin, View):
+    """
+    Create a new vault for the authenticated user.
+    """
+    template_name = "vault_app/vault_form.html"
+    login_url = "user_app:login"
 
+    def get(self, request):
+        form = VaultForm(user=self.request.user)
+
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = VaultForm(request.POST, user=self.request.user)
+
+        if not form.is_valid():
+            return render(request, self.template_name, {"form": form})
+
+        try:
+            vault = VaultService.create(
+                user=request.user,
+                name=form.cleaned_data["name"],
+                description=form.cleaned_data["description"],
+                is_default=form.cleaned_data["is_default"]
+            )
+        except DuplicateVaultExceptions as exc:
+            form.add_error("name", str(exc))
+            return render(request, self.template_name, {"form": form})
+
+        return redirect(
+            "vault_app:detail",
+            username=vault.user.username,
+            vault_slug=vault.slug
+        )
+
+
+class VaultUpdateView(LoginRequiredMixin, VaultObjectMixin, View):
+    """
+    Update a vault owned by the authenticated user.
+    """
+    template_name = "vault_app/vault_form.html"
+    login_url = "user_app:login"
+
+    def get(self, request, *args, **kwargs):
+        vault = self.get_vault()
+        form = VaultForm(user=self.request.user, vault=vault, initial={
+            "name": vault.name,
+            "description": vault.description,
+            "is_default": vault.is_default,
+        })
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        vault = self.get_vault()
+        form = VaultForm(request.POST, user=request.user, vault=vault)
+
+        if not form.is_valid():
+            return render(request, self.template_name, {"form": form, "vault": vault})
+
+        try:
+            updated_vault = VaultService.update(
+                user_id=request.user,
+                data={
+                    "pk": vault.pk,
+                    **form.cleaned_data,
+                }
+            )
+        except DuplicateVaultExceptions as exc:
+            form.add_error("name", str(exc))
+
+            return render(request, self.template_name, {"form": form, "vault": vault})
+
+        return redirect(
+            "vault_app:detail",
+            username=vault.user.username,
+            vault_slug=vault.slug
+        )
+    
